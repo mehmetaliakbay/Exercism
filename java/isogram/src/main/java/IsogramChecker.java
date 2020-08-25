8ab43880f7849f3fcff772740fc708a7d4e87dfc
mehmet_ali_akbay <mhmtaliakbay@gmail.com>
2020-08-25 18:40:07 +0300

    changed with efficient solution

--- java/isogram/src/main/java/IsogramChecker.java
+++ java/isogram/src/main/java/IsogramChecker.java
@@ -1,11 +1,9 @@
 class IsogramChecker {
 
     boolean isIsogram(String phrase) {
-        phrase = phrase.replaceAll("\\s", "");
-        phrase = phrase.replaceAll("-", "").toLowerCase();
-        if (phrase.isEmpty() || phrase.length() == phrase.chars().distinct().count())
-            return true;
-        return false;
+        phrase = phrase.toLowerCase().replaceAll("[\\W]", "");
+       return phrase.isEmpty() || phrase.length() == phrase.chars().distinct().count();
+
     }
 
 }
